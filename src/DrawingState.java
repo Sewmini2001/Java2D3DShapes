26075fb780cb76e1aad095a21bbd1a058b0b85b5
Sewmini2001 <dsvjayakody@students.nsbm.ac.lk>
2023-10-23 03:54:54 +0530

    include function works for (JAR) libraries

---
+++ src/DrawingState.java
@@ -0,0 +1,17 @@
+import java.awt.Shape;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrawingState implements Serializable {
+    private List<Shape> shapes;
+
+    public DrawingState(List<Shape> shapes) {
+        this.shapes = shapes;
+    }
+
+
+    public List<Shape> getShapes() {
+        return shapes;
+    }
+}
